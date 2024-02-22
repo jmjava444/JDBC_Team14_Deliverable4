877f899af83becc98db550a61554396de9da7ad3
Josh <111jmckenzie@gmail.com>
2024-02-21 22:37:45 -0800

    Add existing project files

---
+++ src/ser322/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @authors Joshua McKenzie, Dung Ho, Estevan Perez
+ * Project Deliverable 4
+ * Team 11
+ * SER 322 - Summer Session C
+ * Instructor Moubayed
+ */
+
+package ser322;
+
+import java.io.*;
+import java.sql.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
